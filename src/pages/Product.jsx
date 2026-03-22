f461a0ff8c756a58c449faaf918988520c5d6833
Yousuf7900 <yousufislam7900@gmail.com>
2026-03-23 00:12:37 +0600

    Related products based on cateogyr

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
 
@@ -9,6 +10,7 @@ const Product = () => {
     const { products, currency } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
 
     const fetchProductData = async () => {
         products.map(item => {
@@ -53,8 +55,36 @@ const Product = () => {
                     </div>
                     <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
                     <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+                    <div className="flex flex-col gap-4 my-8">
+                        <p>Select Size</p>
+                        <div className="flex gap-2">
+                            {productData.sizes.map((item, idx) => (
+                                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={idx}>{item}</button>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+                    <hr className="mt-8 sm:w-4/5" />
+                    <div className="text-sm text-gray-500 mt-5 flex-col gap-1">
+                        <p>100% Original product.</p>
+                        <p>Cash on delivery is available on this product.</p>
+                        <p>Easy return and exchange policy within 7 days.</p>
+                    </div>
                 </div>
             </div>
+            {/* Description and Review section */}
+            <div className="mt-20">
+                <div className="flex gap-2">
+                    <b className="border px-5 py-3 text-sm">Description</b>
+                    <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+                </div>
+                <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, totam?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, exercitationem!</p>
+                </div>
+            </div>
+            {/* Display Related products */}
+            <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
         </div>
     ) : <div className="opacity-0">
 
