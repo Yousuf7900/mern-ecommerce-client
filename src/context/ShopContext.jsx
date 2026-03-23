31e29aa8a5a5a473b5e0185c2208984e7b303bf8
Yousuf7900 <yousufislam7900@gmail.com>
2026-03-23 19:22:54 +0600

    onek kichu ek sathe update kora hoice

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -49,6 +49,30 @@ const ShopContextProvider = (props) => {
         return totalCount;
     }
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        return totalAmount;
+    }
+
     const value = {
         products,
         currency,
@@ -59,7 +83,9 @@ const ShopContextProvider = (props) => {
         setShowSearch,
         addToCart,
         cartItems,
-        getCartCount
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
     }
 
     return (
